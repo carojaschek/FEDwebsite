a449208d297dffafbe735070526a1cc340130ad5
carojaschek <74109788+carojaschek@users.noreply.github.com>
2020-11-25 13:55:57 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+/* klikken met de muis op deButton*/
+var feedbackButton = document.querySelector("form h2");
+
+function formHeenEnWeer(){
+    let hetFormulier = document.querySelector("form");
+    hetFormulier.classList.toggle("toonForm");
+}
+
+feedbackButton.addEventListener("click", formHeenEnWeer);
